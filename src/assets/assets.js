a4d626b28dbb5c869183add18add0f88abe2f5aa
samin sadat <saminsadat.cse@gmail.com>
2025-03-01 22:57:29 +0600

    initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,91 @@
+import alitalia from "./alitalia-logo.png";
+import apple_store from "./apple-store.png";
+import arrow_down from "./arrow-down.png";
+import arrow_up from "./arrow-up.png";
+import arrow_up_svg from "./arrow-up.svg";
+import axon_logo from "./axon-logo.png";
+import best_flight from "./best-flight.png";
+import building_icon from "./building-icon.png";
+import clip_2 from "./clip-2.png";
+import clip_3 from "./clip-3.png";
+import clip_image from "./clip-image.png";
+import customization from "./customization.png";
+import dots from "./dots.png";
+import dots2 from "./dots2.png";
+import europe from "./europe.png";
+import expedia_logo from "./expedia-logo.png";
+import google_play from "./google-play.png";
+import jetstar_logo from "./jetstar-logo.png";
+import leaves_icon from "./leaves-icon.png";
+import location from "./location.png";
+import location2 from "./location2.png";
+import logo from "./logo.png";
+import logo2 from "./logo2.png";
+import london from "./london.png";
+import love_icon from "./love-icon.png";
+import mail_icon from "./mail-icon.png";
+import man_smile from "./man-smile.png";
+import mic from "./mic.png";
+import mosque from "./mosque.png";
+import network from "./network.png";
+import plane from "./plane.png";
+import play from "./play.png";
+import qantas_logo from "./qantas-logo.png";
+import rectangle from "./rectangle.png";
+import rome from "./rome.png";
+import route_icon from "./route-icon.png";
+import telegram_icon from "./telegram-icon.png";
+import traveller1 from "./traveller 1.png";
+import traveller2 from "./traveller2.png";
+import vector1 from "./vector1.png";
+import vector2 from "./vector2.png";
+import vetor_logo1 from "./vetor-logo1.png";
+import vetor_logo2 from "./vetor-logo2.png";
+import vetor_logo3 from "./vetor-logo3.png";
+
+export const assets = {
+  alitalia,
+  apple_store,
+  arrow_down,
+  arrow_up,
+  arrow_up_svg,
+  axon_logo,
+  best_flight,
+  building_icon,
+  clip_2,
+  clip_3,
+  clip_image,
+  customization,
+  dots,
+  dots2,
+  europe,
+  expedia_logo,
+  google_play,
+  jetstar_logo,
+  leaves_icon,
+  location,
+  location2,
+  logo,
+  logo2,
+  london,
+  love_icon,
+  mail_icon,
+  man_smile,
+  mic,
+  mosque,
+  network,
+  plane,
+  play,
+  qantas_logo,
+  rectangle,
+  rome,
+  route_icon,
+  telegram_icon,
+  traveller1,
+  traveller2,
+  vector1,
+  vector2,
+  vetor_logo1,
+  vetor_logo2,
+  vetor_logo3,
+};
