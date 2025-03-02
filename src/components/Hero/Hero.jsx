5d5dd5b31b1c5dbb11042532d92ced6b0b98f390
unknown <mahadi15-14178@diu.edu.bd>
2025-03-02 15:37:57 +0600

    added css navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import './Hero.css';
+import { assets } from '../../assets/assets';
 
 const Hero = () => {
-  return <div className="container mx-auto">Hero</div>;
+  return (
+    <>
+      <div className='bg-curve absolute top-0 right-0 -z-1 h-svh'>
+        <img src={assets.clip_image} alt='' />
+        <div className='container mx-auto flex items-center left-0'>hi</div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,32 @@
-import React from "react";
-import { assets } from "../../assets/assets";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import React from 'react';
+import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div className="container w-[1160px] mx-auto">
-      <div className="navbar-list-items flex items-center justify-between pt-[47px] text-[#212832]">
-        <div className="navbar-logo">
-          <Link to="/">
-            <img src={assets.logo} className="w-[115px]" alt="" />
+    <div className='container mx-auto mt-10'>
+      <div className='navbar-list-items flex items-center justify-between text-[#212832]'>
+        <div className='navbar-logo'>
+          <Link to='/'>
+            <img src={assets.logo} className='w-[115px]' alt='' />
           </Link>
         </div>
-        <div className="navbar-menu space-x-16 ml-8 font-medium text-[17px] flex items-center">
-          <a href="">Desitnations</a>
-          <a href="">Hotels</a>
-          <a href="">Flights</a>
-          <a href="">Bookings</a>
-          <a href="">Login</a>
-          <div className="navbar-buttons space-x-10 font-medium text-[17px]">
-            <button className="border border-[#212832] w-[102px] h-[40px] rounded-[5px] cursor-pointer">
+        <div className='navbar-menu space-x-14 ml-8 font-medium text-[17px] flex items-center'>
+          <a href=''>Desitnations</a>
+          <a href=''>Hotels</a>
+          <a href=''>Flights</a>
+          <a href=''>Bookings</a>
+          <a href=''>Login</a>
+          <div className='navbar-buttons space-x-10 font-medium text-[17px]'>
+            <button className='border border-[#212832] w-[102px] h-[40px] rounded-[5px] cursor-pointer'>
               Sign up
             </button>
-            <select name="" id="" className="cursor-pointer">
-              <option value="eng">EN</option>
-              <option value="bn">BN</option>
+            <select name='' id='' className='cursor-pointer border-none'>
+              <option value='eng' className='text-red-400'>
+                EN
+              </option>
+              <option value='bn'>BN</option>
             </select>
           </div>
         </div>
